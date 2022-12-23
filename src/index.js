30eeac4e4ac47c5e42ad82c547dd16a7edf1a3fd
Balaguru <75660556+Balagurusiva@users.noreply.github.com>
2022-12-23 20:16:28 +0530

    Delete index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "../src/app";
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-   <App />
-)
